75d9afa2dee30f563ff9ad44b04bcdacaa461b86
Sam Garcia <samuelcgarcia.3@gmail.com>
2021-10-06 16:57:31 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting the success of a film using historical data',
+    author='Samuel Garcia',
+    license='MIT',
+)
